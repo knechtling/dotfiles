0db1a8c906997e1bc7d9c98bdd3a7de39a9de83e
Anton <anton.liepelt@gmx.de>
2025-02-21 09:40:26 +0100

    now use lazy.nvim in testing

---
+++ dot-config/nvim/lua/plugins/snacks-images.lua
@@ -0,0 +1,90 @@
+-- lazy.nvim
+return {
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      image = {
+        formats = {
+          "png",
+          "jpg",
+          "jpeg",
+          "gif",
+          "bmp",
+          "webp",
+          "tiff",
+          "heic",
+          "avif",
+          "mp4",
+          "mov",
+          "avi",
+          "mkv",
+          "webm",
+          "pdf",
+        },
+        force = false, -- try displaying the image, even if the terminal does not support it
+        doc = {
+          -- enable image viewer for documents
+          -- a treesitter parser must be available for the enabled languages.
+          -- supported language injections: markdown, html
+          enabled = true,
+          -- render the image inline in the buffer
+          -- if your env doesn't support unicode placeholders, this will be disabled
+          -- takes precedence over `opts.float` on supported terminals
+          inline = true,
+          -- render the image in a floating window
+          -- only used if `opts.inline` is disabled
+          float = true,
+          max_width = 40,
+          max_height = 20,
+          -- Set to `true`, to conceal the image text when rendering inline.
+          -- (experimental)
+          conceal = false,
+        },
+        img_dirs = { "img", "images", "assets", "static", "public", "media", "attachments" },
+        -- window options applied to windows displaying image buffers
+        -- an image buffer is a buffer with `filetype=image`
+        wo = {
+          wrap = false,
+          number = false,
+          relativenumber = false,
+          cursorcolumn = false,
+          signcolumn = "no",
+          foldcolumn = "0",
+          list = false,
+          spell = false,
+          statuscolumn = "",
+        },
+        cache = vim.fn.stdpath("cache") .. "/snacks/image",
+        debug = {
+          request = false,
+          convert = false,
+          placement = false,
+        },
+        env = {},
+        ---@class snacks.image.convert.Config
+        convert = {
+          notify = true, -- show a notification on error
+          math = {
+            font_size = "Large", -- see https://www.sascha-frank.com/latex-font-size.html
+            -- for latex documents, the doc packages are included automatically,
+            -- but you can add more packages here. Useful for markdown documents.
+            packages = { "amsmath", "amssymb", "amsfonts", "amscd", "mathtools", "physics", "siunitx", "mhchem" },
+          },
+          ---@type snacks.image.args
+          mermaid = function()
+            local theme = vim.o.background == "light" and "neutral" or "dark"
+            return { "-i", "{src}", "-o", "{file}", "-b", "transparent", "-t", theme, "-s", "{scale}" }
+          end,
+          ---@type table<string,snacks.image.args>
+          magick = {
+            default = { "{src}[0]", "-scale", "1920x1080>" }, -- default for raster images
+            vector = { "-density", 192, "{src}[0]" }, -- used by vector images like svg
+            math = { "-density", 192, "{src}[0]", "-trim" },
+            pdf = { "-density", 192, "{src}[0]", "-background", "white", "-alpha", "remove", "-trim" },
+          },
+        },
+      },
+    },
+  },
+}
