1a1e3217b2905fce9568d68669d4e96650792cb6
Anton <anton.liepelt@gmx.de>
2025-04-02 23:17:51 +0200

    add autocmd for xdefault filetypes

--- dot-config/nvim/lua/config/autocmds.lua
+++ dot-config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Set filetype for .Xresources and .Xdefaults files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { ".Xresources", ".Xdefaults", "xresources", "xdefaults" },
+  desc = "Set filetype for Xresources/Xdefaults",
+  callback = function()
+    vim.bo.filetype = "xdefaults"
+  end,
+})
