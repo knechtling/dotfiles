35b873181869cf10b8005878c732dd0d5a25f73c
Anton Liepelt <al@imunixx.de>
2025-04-17 08:13:19 +0200

    add bulletpoints to wiki

--- dot-config/nvim/lua/plugins/vimwiki.lua
+++ dot-config/nvim/lua/plugins/vimwiki.lua
@@ -5,4 +5,16 @@ return {
       vim.g.wiki_root = "~/vimwiki/text/" -- wiki.vim configuration goes here, e.g.
     end,
   },
+  {
+    "bullets-vim/bullets.vim",
+    ft = { "markdown", "text", "gitcommit", "scratch" },
+    init = function()
+      vim.g.bullets_enabled_file_types = {
+        "markdown",
+        "text",
+        "gitcommit",
+        "scratch",
+      }
+    end,
+  },
 }
